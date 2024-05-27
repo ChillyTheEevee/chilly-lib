27453630fb7118675d23f98a7158fb1d0ccbeb95
Daniel <myersdan@studentmail.conroeisd.net>
2024-05-27 12:43:04 -0500

    Wrote javadocs for the Config API

--- src/main/java/world/sc2/config/ConfigManager.java
+++ src/main/java/world/sc2/config/ConfigManager.java
@@ -6,21 +6,44 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
-//All credit to spigotmc.org user Bimmr for this manager
+
+/**
+ * A manager for all {@link Config} related tasks. Individual plugins must construct this ConfigManager. There should
+ * only be one instance of ConfigManager per plugin, and that instance should be passed around using dependency
+ * injection.
+ * @author Bimmr
+ */
 public class ConfigManager {
 
     private final JavaPlugin plugin;
-    private final HashMap<String, Config> configs = new HashMap<>();
+    private final Map<String, Config> configs = new HashMap<>();
 
+    /**
+     * Constructs a new ConfigManager for the given {@link JavaPlugin}. There should only be one ConfigManager per
+     * plugin, and this instance of ConfigManager should be passed around using dependency injection.
+     * @param plugin The plugin to create this ConfigManager for.
+     */
     public ConfigManager(JavaPlugin plugin) {
         this.plugin = plugin;
     }
 
-    public HashMap<String, Config> getConfigs() {
+    /**
+     * Returns the internal Map of all {@link Config} files registered with this ConfigManager.
+     * @return the internal Map of all Config files registered with this ConfigManager.
+     */
+    public Map<String, Config> getConfigs() {
         return configs;
     }
 
+    /**
+     * Returns the {@link Config} with the given name. If no Config registered with this ConfigManager has the given
+     * name, then a new Config is created and registered with this ConfigManager using the given name.
+     * @param name The name of the Config to return.
+     * @return The Config value stored within the Config map in this ConfigManager for the given name, or a new Config
+     * with the given name if it does not exist within that map.
+     */
     public Config getConfig(String name) {
         if (!configs.containsKey(name))
             configs.put(name, new Config(plugin, name));
@@ -28,16 +51,28 @@ public class ConfigManager {
         return configs.get(name);
     }
 
+    /**
+     * Reloads the {@link Config} with the given name. Exactly the same as instead calling getConfig(name).reload().
+     * @param name The name of this Config.
+     * @return The Config that was reloaded.
+     */
     public Config reloadConfig(String name) {
         return getConfig(name).reload();
     }
 
+    /**
+     * Reloads every {@link Config} registered with this ConfigManager by calling {@link Config#reload()}.
+     */
     public void reloadConfigs() {
-        for (String config : configs.keySet()){
-            configs.get(config).reload();
+        for (Config config : configs.values()){
+            config.reload();
         }
     }
 
+    /**
+     * Saves every {@link Config} registered within this ConfigManager by internally calling saveConfig(name) for each
+     * individual Config name.
+     */
     public void saveConfigs() {
         for (String config : configs.keySet()) {
             saveConfig(config);
@@ -72,7 +107,8 @@ public class ConfigManager {
     }
 
     /**
-     * Updates a config by comparing in the plugin's assigned data folder by copying over data from the jar file
+     * Updates a Config by comparing it to the plugin in the plugin's assigned data folder and copying over any missing
+     * data from the jar file.
      * @param name The path to the config from the plugin's assigned data file
      */
     public void updateConfig(String name) {
@@ -85,7 +121,8 @@ public class ConfigManager {
     }
 
     /**
-     * Saves and updates the given config
+     * Saves and updates the given config within this ConfigManager. equivalent to calling saveConfig(name) and then
+     * updateConfig(name).
      * @param name The path to the config from the plugin's assigned data file
      */
     public void saveAndUpdateConfig(String name) {
